980b91b12c4a7674529654680e40d5ce05b2a5dc
Jeffkungu <Jeffkungu@gmail.com>
2018-02-20 15:42:25 +0300

    [Feature] adds shared preference.

---
+++ app/src/main/java/com/sqisland/tutorial/recipes/data/local/SharedPreferencesFavourites.java
@@ -0,0 +1,36 @@
+package com.sqisland.tutorial.recipes.data.local;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Jeffkungu on 20/02/2018.
+ */
+
+public class SharedPreferencesFavourites {
+    private final SharedPreferences preferences;
+
+    public SharedPreferencesFavourites(Context context) {
+        preferences = context.getSharedPreferences("favourites.xml", Context.MODE_PRIVATE);
+    }
+
+    public boolean get(String id) {
+        return  preferences.getBoolean(id, false);
+    }
+
+    public void put(String id, boolean favourite) {
+        SharedPreferences.Editor editor = preferences.edit();
+        if (favourite) {
+            editor.putBoolean(id, true);
+        } else {
+            editor.remove(id);
+        }
+        editor.apply();
+    }
+
+    public boolean toggle(String id) {
+        boolean favourite = get(id);
+        put(id, !favourite);
+        return !favourite;
+    }
+}
