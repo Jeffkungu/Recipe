040efa5b231bcef87f5aa78c264e4752c5f44829
Jeffkungu <Jeffkungu@gmail.com>
2018-03-12 16:59:03 +0300

    [Feature] adds implementation of the RecipeContract class methods in the Activity.

--- app/src/main/java/com/sqisland/tutorial/recipes/ui/recipe/RecipeActivity.java
+++ app/src/main/java/com/sqisland/tutorial/recipes/ui/recipe/RecipeActivity.java
@@ -5,59 +5,69 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.sqisland.tutorial.recipes.R;
 import com.sqisland.tutorial.recipes.data.local.Favourites;
 import com.sqisland.tutorial.recipes.data.local.RecipeStore;
-import com.sqisland.tutorial.recipes.data.model.Recipe;
 import com.sqisland.tutorial.recipes.injection.RecipeApplication;
 
 /**
  * Created by Jeffkungu on 19/02/2018.
  */
 
-public class RecipeActivity extends AppCompatActivity {
+public class RecipeActivity extends AppCompatActivity implements RecipeContract.View {
     public static final String KEY_ID = "id";
-
     private TextView title, description;
+    RecipeContract.Listener listener;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        // Step 1: Set up the UI
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
         title = (TextView) findViewById(R.id.tvTitlle);
         description = (TextView) findViewById(R.id.tvDescription);
 
+        // Step 2: Load recipe from RecipeStore
         RecipeStore store = new RecipeStore(this, "recipes");
         String id = getIntent().getStringExtra(KEY_ID);
-        final Recipe recipe = store.getRecipe(id);
-
-        if (id == null) {
-            title.setVisibility(View.GONE);
-            description.setText(R.string.recipe_not_found);
-            return;
-        }
-
         RecipeApplication app = (RecipeApplication) getApplication();
         final Favourites favourites = app.getFavourites();
-        boolean favourite = favourites.get(recipe.id);
+        final RecipePresenter presenter = new RecipePresenter(store, this, favourites); // Has replaced the logic for loading recipe from RecipeStore.
+        presenter.loadRecipe(id);
 
-        title.setText(recipe.title);
-        title.setSelected(favourite);
+        // Step 3: If recipe is null, show error. (Its handled in the RecipePresenter.loadRecipe method)
+
+        // Step 4: If recipe not null, show recipe (Its handled in the RecipePresenter.loadRecipe method)
+
+        // Step 5: When title is clicked, toggle favourites
         title.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                boolean result = favourites.toggle(recipe.id);
-                title.setSelected(result);
-                if (result) {
-                    Toast.makeText(getApplicationContext(), "You have selected " + recipe.title + " as favourite", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "You have un-selected " + recipe.title, Toast.LENGTH_LONG).show();
-                }
+                listener.toggleFavourite();
             }
         });
-        description.setText(recipe.description);
+    }
+
+    @Override
+    public void showRecipeNotFoundError() {
+        title.setVisibility(View.GONE);
+        description.setText(R.string.recipe_not_found);
+    }
+
+    @Override
+    public void setTitle(String text) {
+        title.setText(text);
+    }
+
+    @Override
+    public void setDescription(String text) {
+        description.setText(text);
+    }
+
+    @Override
+    public void setFavourites(boolean favourite) {
+        title.setSelected(favourite);
     }
 }
